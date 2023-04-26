db77e096feafcc877ee6ebc9e6c39917e72cbd93
LucasAraujo1953 <130591551+LucasAraujo1953@users.noreply.github.com>
2023-04-26 00:04:16 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista2Ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int a;
+            int h;
+
+                Console.WriteLine("Vamos calcular a área de um terreno retângular ");
+            Console.Write("Digite sua base: ");
+            b=int.Parse(Console.ReadLine());
+            Console.Write("Digite sua altura: ");
+            a=int.Parse(Console.ReadLine());
+
+            h = a * b;
+            Console.WriteLine("Seu terreno tem área de {0}m²", h) ;
+            if (h >= 100)
+                Console.WriteLine("Seu terreno é grande!");
+            else
+                Console.WriteLine("Seu terreno é pequeno!");
+        }
+    }
+}
